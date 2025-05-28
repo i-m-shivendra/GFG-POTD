e6b3317ede83a46b788ac8c2a9850abfcdcb85db
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-05-28 08:00:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
+++ Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
@@ -1,90 +1,45 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 
 class Solution {
-    
   private:
-     void dfs(int node,  vector<vector<int>>& adjList, vector<int>& 
-     visited, vector<int>& temp){
-         visited[node] = 1;
-         temp.push_back(node);
-         
-         for(int i=0;i<adjList[node].size(); i++){
-             if(!visited[adjList[node][i]]){
-                 dfs(adjList[node][i], adjList, visited, temp);
-             }
-         }
-     }
+  
+  void dfs(int node, vector<vector<int>>& adjList,  
+  vector<int>& visited, vector<int>& temp){
+      visited[node] = 1;
+      temp.push_back(node);
+      
+      for(int i=0;i<adjList[node].size();i++){
+          if(!visited[adjList[node][i]]){
+              dfs(adjList[node][i], adjList, visited, temp);
+          }
+      }
+      
+  }
   public:
     vector<vector<int>> getComponents(int V, vector<vector<int>>& edges) {
-        // Initialisation of adjacency list
-        vector<vector<int>> adjList(V);
-        
-        //Creation of adjacency list
-        for(auto it : edges){
-            adjList[it[0]].push_back(it[1]);
-            adjList[it[1]].push_back(it[0]);
-        }
-        
-        //Creating a visited array
-        vector<int> visited(V, 0);
-        
-        //Creating an answer vector
-        vector<vector<int>> ans;
-        
-        //Finding all the connected components
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                vector<int> temp;
-                dfs(i, adjList, visited, temp);
-                ans.push_back(temp);
-            }
-        }
-        return ans;
+      
+      //Initialization of adjacency list
+      vector<vector<int>> adjList(V);
+      
+      //Creation of adjacency list
+      for(auto it : edges){
+          adjList[it[0]].push_back(it[1]);
+          adjList[it[1]].push_back(it[0]);
+      }
+      
+      //Creating a visited array
+      vector<int> visited(V, 0);
+      
+      //Creating an answer vector
+      vector<vector<int>> ans;
+      
+      for(int i=0;i<V;i++){
+          if(!visited[i]){
+              vector <int> temp;
+              dfs(i, adjList, visited, temp);
+              ans.push_back(temp);
+          }
+      }
+    
+      return ans;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore();
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-        cin.ignore();
-        vector<vector<int>> edges;
-        for (int i = 1; i <= E; i++) {
-            int u, v;
-            cin >> u >> v;
-            edges.push_back({u, v});
-        }
-
-        Solution obj;
-        vector<vector<int>> res = obj.getComponents(V, edges);
-
-        for (int i = 0; i < res.size(); i++) {
-            sort(res[i].begin(), res[i].end());
-        }
-        sort(res.begin(), res.end());
-        for (const auto &component : res) {
-            for (int node : component) {
-                cout << node << " ";
-            }
-            cout << endl;
-        }
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
