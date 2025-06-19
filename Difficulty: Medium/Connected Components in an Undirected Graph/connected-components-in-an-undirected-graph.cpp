0451b1b1c6c84506f559e1e343a0025e4bb81b17
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-06-19 17:52:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
+++ Difficulty: Medium/Connected Components in an Undirected Graph/connected-components-in-an-undirected-graph.cpp
@@ -1,37 +1,33 @@
 
 class Solution {
   private:
-     void dfs(int node,vector<vector<int>>& adjList, vector<int>&
-     visited, vector<int>& temp){
-         visited[node] = 1;
-         temp.push_back(node);
-         
-         for(int i=0;i<adjList[node].size();i++){
-             if(!visited[adjList[node][i]]){
-                 dfs(adjList[node][i],adjList,visited,temp);
-             }
-         }
-         
-     }
-     
-  public:
-    vector<vector<int>> getComponents(int V, 
-    vector<vector<int>>& edges) {
+    void dfs(int node,vector<vector<int>>& adjList,
+    vector<int>& visited,vector<int>& temp){
+        visited[node] = true;
+        temp.push_back(node);
         
-        //Initiliasation of adjList
+        for(auto it : adjList[node]){
+            if(!visited[it]){
+                dfs(it,adjList,visited,temp);
+            }
+        }
+    }
+  public:
+    vector<vector<int>> getComponents(int V, vector<vector<int>>& edges) {
+        //Create an adjacency List
         vector<vector<int>> adjList(V);
         
-        //Creation of adjList
+        //Initialisation of adjacency list
         for(auto it : edges){
             adjList[it[0]].push_back(it[1]);
             adjList[it[1]].push_back(it[0]);
         }
         
-        //Creating a visited array
-        vector<int> visited(V,0);
-        
-        //Creating an answer vector
+        //Create an answer vector
         vector<vector<int>> ans;
+        
+        //Create a visited array
+        vector<int> visited(V,0);
         
         for(int i=0;i<V;i++){
             if(!visited[i]){
