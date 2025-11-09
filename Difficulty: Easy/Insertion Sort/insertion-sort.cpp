7c37b5e7d26bc414b18fdb0dd2d947703ea4012f
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-11-09 17:34:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -6,9 +6,9 @@ class Solution {
             int temp = arr[i];
             int j = i-1;
             
-            while(j>=0 && arr[j]>temp){
+            while(j >= 0 && arr[j] > temp){
                 arr[j+1] = arr[j];
-                j=j-1;
+                j = j-1;
             }
             arr[j+1] = temp;
         }
