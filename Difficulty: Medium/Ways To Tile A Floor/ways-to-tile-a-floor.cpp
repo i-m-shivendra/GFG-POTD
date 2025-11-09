5324f6e02abcdd96044d7c58adb73603a6731f73
Shivendra Pratap Singh <108616385+i-m-shivendra@users.noreply.github.com>
2025-11-09 17:51:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways To Tile A Floor/ways-to-tile-a-floor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfWays(int n) {
+        if(n == 0 || n == 1) return 1;
+
+        int prev2 = 1; 
+        int prev1 = 1; 
+        int curr;
+
+        for(int i = 2; i <= n; i++) {
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
